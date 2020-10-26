4e9c507544ed1417317b5c67aee082d9b1fa6c7d
Barba <Barba@192.168.0.192>
2020-10-26 16:45:54 -0300

    Teoria 26/10 Limitado

---
+++ Concurrente/src/TeoriaConcurrente/Productor.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package TeoriaConcurrente;
+
+/**
+ *
+ * @author Barba
+ */
+public class Productor extends Persona implements Runnable {
+    
+    
+    public Productor(int nom, Buffer buf){
+        super(nom, buf);
+    }
+    
+    @Override
+    public void run() {
+       while(true){
+           buffer.agregarElemento(nombre);
+       }
+    }
+
+}
